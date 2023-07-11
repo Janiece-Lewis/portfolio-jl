c9bf9508ac646a1133b29cbc5513bc786dafa60f
Janiece-Lewis <jlewilewi@gmail.com>
2023-07-11 10:59:34 -0400

    updated avatar image

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
 import React from "react";
+import avatar from '../images/avatar.png'
 import c from '../images/c.jpg'
+
 
 const About= () =>{
     return(
-    // <div id='about' >
+    <div className="">
 
-<div class="flex items-center space-x-4 h-20 max-w-[1240px]">
-    <img class="w-10 h-10 rounded-full" src={c} alt=""/>
-    <div class="font-medium dark:text-white">
-        <div>Janiece Lewis</div>
+<div class="flex items-center space-x-8 h-28 max-w-[1240px] px-5">
+    <img class="w-50 h-50 rounded-full" src={avatar} alt=""/>
+    <div class="font-medium dark:text-white text-2xl">
+        <div class='text-white'>Janiece Lewis</div>
         <div class="text-sm text-gray-500 dark:text-gray-400">fullstack web dev</div>
     </div>
 </div>
